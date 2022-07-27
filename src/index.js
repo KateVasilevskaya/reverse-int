51acf963bd9d7b3f592a699012e51445329b803c
KateVasilevskaya <7091764@gmail.com>
2022-07-27 13:55:28 +0200

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newN = Math.abs(n);
+    let str = newN.toString();
+    let array = [...str].reverse().join("");
+    let rez = Number(array);
+    return rez;
+};
